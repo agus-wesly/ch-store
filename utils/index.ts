51b0eaa1385ee7334f415af77ad52128e676e676
agustforbussines <agustchannel@gmail.com>
2023-01-03 20:57:13 +0700

    fixing banner and overflow-x

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 const fetchers = async (url: string, revalidate?: number) => {
   const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
-    next: { revalidate: !revalidate ? false : 60 },
+    next: { revalidate: !revalidate ? false : revalidate },
   });
   const data: Product[] = await resp.json();
 
