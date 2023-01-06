f2d679b942653421d6a918fa8ffbb930c76e380f
agustforbussines <agustchannel@gmail.com>
2023-01-06 20:37:29 +0700

    testing middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,14 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ req, token }) => {
+      //console.log("req", req);
+      console.log("token", token);
+      if (token) return true; // If there is a token, the user is authenticated
+      return false;
+    },
+  },
+});
 
 export const config = { matcher: ["/cart"] };
